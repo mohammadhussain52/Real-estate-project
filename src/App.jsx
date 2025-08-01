ee8776c98684df4b9670092ce31ab8659d1e4b5b
mohammadhussain52 <mhussain8440@gmail.com>
2025-08-01 18:48:48 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Components/Header";
 import About from "./Components/About";
 import Projects from "./Components/Projects";
 import Testimonials from "./Components/Testimonials";
 import Contact from "./Components/Contact";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Footer from "./Components/Footer";
+import { FaArrowUp } from "react-icons/fa";
 
 const App = () => {
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const headers = document.querySelector("#Header");
+  const [showBtn, setShowBtn] = useState("myBtn none");
+  window.onscroll = function () {
+    scrollFunction();
+  };
 
-  //     if (window.scrollY > 100) {
-  //       headers?.classList.add("sticky-active");
-  //       headers?.classList.remove("sticky-soon");
-  //     } else if (window.scrollY > 10) {
-  //       headers?.classList.add("sticky-soon");
-  //       headers?.classList.remove("sticky-active");
-  //     } else {
-  //       headers?.classList.remove("sticky-active");
-  //       headers?.classList.remove("sticky-soon");
-  //     }
-  //   };
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      setShowBtn("myBtn");
+    } else {
+      setShowBtn("none");
+    }
+  }
 
-  //   window.addEventListener("scroll", handleScroll);
-  //   return () => window.removeEventListener("scroll", handleScroll);
-  // }, []);
+  function topFunction() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
 
   return (
     <div className="w-full overflow-hidden">
@@ -38,6 +41,14 @@ const App = () => {
       <Testimonials />
       <Contact />
       <Footer />
+      <button
+        onClick={topFunction}
+        id="myBtn"
+        className={showBtn}
+        title="Go to Top"
+      >
+        <FaArrowUp />
+      </button>
     </div>
   );
 };
